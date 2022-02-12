dd055aae5678bfca6cd37d15a7691860bea0a844
x4th <stefan@bajic.io>
2022-02-12 02:21:59 +0100

    Initial commit

---
+++ src/week-1/pset-0/count-long-subarray.ts
@@ -0,0 +1,10 @@
+/**
+ *
+ * @param A list of positive integers
+ * @returns number of longest increasing subarrays of A
+ */
+function countLongSubarray(A): number {
+  let count = 0;
+  // TODO: implement
+  return count;
+}
